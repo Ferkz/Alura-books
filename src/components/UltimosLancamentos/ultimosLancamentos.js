7f8e2829f148e8edf5b6ed000ecb2d2ae92d8461
Ferkz <fermon1952@gmail.com>
2023-03-30 11:41:33 -0400

    alura-books-update

--- src/components/UltimosLancamentos/ultimosLancamentos.js
+++ src/components/UltimosLancamentos/ultimosLancamentos.js
@@ -1,22 +1,14 @@
 import {livros} from './dadosUltimosLancamentos'
 import styled from 'styled-components'
-
+import { Titulo }  from '../titulo/index'
+import CardRecomenda from '../cardRecomenda/index'
+import imagemLivro from '../../img/livro2.png'
 
 const UltimosLancamentosContainer = styled.section`
     background-color: #EBECEE;
     padding-bottom: 20px;
     display: flex;
     flex-direction: column;
-`
-
-const Titulo = styled.h2`
-    width: 100%;
-    padding: 30px 0;
-    background-color: #FFF;
-    color: #EB9B00;
-    font-size: 36px;
-    text-align: center;
-    margin: 0;
 `
 
 const NovosLivrosContainer = styled.div`
@@ -35,6 +27,12 @@ function UltimosLancamentos() {
             <img src={livro.src}/>
         ))}
         </NovosLivrosContainer>
+        <CardRecomenda
+                titulo="Talvez você se interesse por"
+                subtitulo="Angular 11"
+                descricao="Construindo uma aplicação com a plataforma Google"
+                img={imagemLivro}
+            />
         </UltimosLancamentosContainer>
     )
 }
